d3a9c6bc74a291efd4b49d40f7412cecc8a9c83f
peter <pero@eucsyd.dk>
2025-03-04 13:56:16 +0100

    Første runde - rå turtle kommandoer

---
+++ polygon.py
@@ -0,0 +1,24 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.pu()
+t.goto(0,0)
+t.pd()
+
+t.fd(100)
+t.lt(51.4)
+t.fd(100)
+t.lt(51.4)
+t.fd(100)
+t.lt(51.4)
+t.fd(100)
+t.lt(51.4)
+t.fd(100)
+t.lt(51.4)
+t.fd(100)
+t.lt(51.4)
+t.fd(100)
+t.lt(51.4)
+
+turtle.done()
